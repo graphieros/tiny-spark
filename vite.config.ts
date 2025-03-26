139c297caac9ebea7b54f64878d42266c62bc757
graphieros <alec.lloyd.probert@gmail.com>
2025-03-26 21:47:19 +0100

    Add readme in dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
 import { defineConfig } from 'vite'
+import { copyFileSync } from 'node:fs'
+import { resolve } from "path";
 
 export default defineConfig({
+  plugins: [
+    {
+      name: "copy-readme",
+      apply: "build",
+      closeBundle() {
+        const source = resolve(__dirname, "README.md");
+        const destination = resolve(__dirname, "dist/README.md");
+        copyFileSync(source, destination);
+        console.log("README.md copied to dist/");
+      },
+    },
+  ],
   build: {
     lib: {
       entry: 'src/index.ts',
