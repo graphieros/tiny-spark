1dbd80b54b71c9d0b7c9247110ae6f42f485f1fd
graphieros <alec.lloyd.probert@gmail.com>
2025-03-26 22:00:58 +0100

    Remove readme inclusion

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,18 @@ import { copyFileSync } from 'node:fs'
 import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [
-    {
-      name: "copy-readme",
-      apply: "build",
-      closeBundle() {
-        const source = resolve(__dirname, "README.md");
-        const destination = resolve(__dirname, "dist/README.md");
-        copyFileSync(source, destination);
-        console.log("README.md copied to dist/");
-      },
-    },
-  ],
+  // plugins: [
+  //   {
+  //     name: "copy-readme",
+  //     apply: "build",
+  //     closeBundle() {
+  //       const source = resolve(__dirname, "README.md");
+  //       const destination = resolve(__dirname, "dist/README.md");
+  //       copyFileSync(source, destination);
+  //       console.log("README.md copied to dist/");
+  //     },
+  //   },
+  // ],
   build: {
     lib: {
       entry: 'src/index.ts',
